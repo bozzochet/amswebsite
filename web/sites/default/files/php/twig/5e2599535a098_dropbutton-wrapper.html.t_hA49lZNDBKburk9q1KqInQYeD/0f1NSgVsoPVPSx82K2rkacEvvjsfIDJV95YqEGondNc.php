c8f9f1d05c22f6da1685921cd629c3400c0f0488
bozzochet <matteo.duranti@infn.it>
2020-01-20 12:29:22 +0000

    imported backup

---
+++ web/sites/default/files/php/twig/5e2599535a098_dropbutton-wrapper.html.t_hA49lZNDBKburk9q1KqInQYeD/0f1NSgVsoPVPSx82K2rkacEvvjsfIDJV95YqEGondNc.php
@@ -0,0 +1,100 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* core/modules/system/templates/dropbutton-wrapper.html.twig */
+class __TwigTemplate_efc2c068204571a1dcd8f8728855967ed8621aa8e1cac8c5cfddfa8810d443f0 extends \Twig\Template
+{
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $tags = ["if" => 14, "spaceless" => 15];
+        $filters = ["escape" => 18];
+        $functions = [];
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['if', 'spaceless'],
+                ['escape'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        // line 14
+        if (($context["children"] ?? null)) {
+            // line 15
+            echo "  ";
+            ob_start(function () { return ''; });
+            // line 16
+            echo "    <div class=\"dropbutton-wrapper\">
+      <div class=\"dropbutton-widget\">
+        ";
+            // line 18
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["children"] ?? null)), "html", null, true);
+            echo "
+      </div>
+    </div>
+  ";
+            echo trim(preg_replace('/>\s+</', '><', ob_get_clean()));
+        }
+    }
+
+    public function getTemplateName()
+    {
+        return "core/modules/system/templates/dropbutton-wrapper.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  64 => 18,  60 => 16,  57 => 15,  55 => 14,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "core/modules/system/templates/dropbutton-wrapper.html.twig", "/var/www/html/amswebsite/web/core/modules/system/templates/dropbutton-wrapper.html.twig");
+    }
+}
