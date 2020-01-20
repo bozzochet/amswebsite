64e7e5dfca953054afe6c10098eb57733d1261f3
bozzochet <matteo.duranti@infn.it>
2020-01-20 12:21:59 +0000

    installed backup_migrate

---
+++ web/sites/default/files/php/twig/5e2599535a098_progress-bar.html.twig_erFaeEO_s2P5M3BGHDepyo1RS/Mzd9xtY0obD_LIn1_pFiP1WXbmIlsyZgOeRtoBc2-Mc.php
@@ -0,0 +1,108 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* core/modules/system/templates/progress-bar.html.twig */
+class __TwigTemplate_e2a00f94a73a7a1a2bb297b9a9384b95e20ca35aaef4b8729d75daf032c9b852 extends \Twig\Template
+{
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $tags = ["if" => 17];
+        $filters = ["escape" => 18];
+        $functions = [];
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['if'],
+                ['escape'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        // line 16
+        echo "<div class=\"progress\" data-drupal-progress>
+  ";
+        // line 17
+        if (($context["label"] ?? null)) {
+            // line 18
+            echo "    <div class=\"progress__label\">";
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["label"] ?? null)), "html", null, true);
+            echo "</div>
+  ";
+        }
+        // line 20
+        echo "  <div class=\"progress__track\"><div class=\"progress__bar\" style=\"width: ";
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["percent"] ?? null)), "html", null, true);
+        echo "%\"></div></div>
+  <div class=\"progress__percentage\">";
+        // line 21
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["percent"] ?? null)), "html", null, true);
+        echo "%</div>
+  <div class=\"progress__description\">";
+        // line 22
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["message"] ?? null)), "html", null, true);
+        echo "</div>
+</div>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "core/modules/system/templates/progress-bar.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  75 => 22,  71 => 21,  66 => 20,  60 => 18,  58 => 17,  55 => 16,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "core/modules/system/templates/progress-bar.html.twig", "/var/www/html/amswebsite/web/core/modules/system/templates/progress-bar.html.twig");
+    }
+}
